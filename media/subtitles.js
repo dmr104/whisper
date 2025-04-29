6c26b4879502727c0aa53743f7b50f6572d6875c
David Roderick <angel.of.north@gmail.com>
2025-04-29 02:59:35 +0100

    applyFormat now works.  getting there

--- media/subtitles.js
+++ media/subtitles.js
@@ -22,8 +22,20 @@
 	// This following variable is to ensure that the first time we click on the first box, the initial
 	// div that gets written to the undoStack will only be written once, and initially.
 	let singletonVarOnMouseUp = false;
+
 	// To determine whether the text within a box has been altered;
 	let boxIsChanged = false;
+
+	//The following is to regulate the first ever which: previous to set the ball rolling.  It is necessary 
+	// because to the logic surrounding singletonVarOnMouseUp
+    let firstTimeThroughSingleton = true;
+	
+	// The following records which div was last formatted.  Is used in conjunction with which: "succession"
+	// to determine whether to omit the div with which: "previous"
+	let lastDivFormatted;
+
+	// Likewise the following is to record as a global state the last record of what the last formatted which: was equal to
+	let lastWhichParentDiv;
 
 	// the following flag is used to control the logic of the change style button press
 	let changedView=false;	
@@ -57,11 +69,47 @@
 	});
 
 	function applyFormat(type) {
+		// Get the element where the cursor is located
 		const selection = window.getSelection();
+
 		if (!selection.rangeCount) {
 			return;
 		}
 		const range = selection.getRangeAt(0);
+
+		let parentDiv = range.commonAncestorContainer;
+		// Traverse up to find the nearest div
+		while (parentDiv && parentDiv.nodeType !== Node.ELEMENT_NODE) {
+			parentDiv = parentDiv.parentNode;
+		}
+
+		// The following is to incorporate the logic we have already set up to establish what happens on mouseup first time through.
+		if (firstTimeThroughSingleton) {
+			undoStack.push({id: parentDiv.id, which: "previous", blobHTML: parentDiv.outerHTML});
+			firstTimeThroughSingleton = false;
+			lastDivFormatted = Number(parentDiv.id);
+			lastWhichParentDiv = "previous";		
+		} else {
+			console.log('lastDivFormatted', lastDivFormatted);
+			console.log('Number(parentDiv.id)', Number(parentDiv.id));
+			console.log('which is ', lastWhichParentDiv);
+
+			// Check whether the parent is a div and get its ID.  And only do the following if the last id doesn't match 
+			// the present one and the last which: succession.  This is to ensure that which: previous will match if the ids do 
+			if (parentDiv && parentDiv.id && (lastDivFormatted !== Number(parentDiv.id)) && (lastWhichParentDiv === "succession")) {			
+				// Make sure we record this change in order to give the possibility of reversing it.
+				undoStack.push({id: parentDiv.id, which: "previous", blobHTML: parentDiv.outerHTML});
+			}
+		}
+ 
+		if (lastDivFormatted !== Number(parentDiv.id)) {
+			lastDivFormatted = Number(parentDiv.id);
+		}
+
+		if (lastWhichParentDiv === "previous") {
+			lastWhichParentDiv = "succession";
+		}
+
 		const selectedText = range.toString();
 	
 		if (selectedText.length === 0) {
@@ -93,13 +141,19 @@
 		range.setEndAfter(wrapper);
 		selection.removeAllRanges();
 		selection.addRange(range);
-		
-		
-		// Make sure we record this change in order to give the possibility of reversing it.
-		undoStack.push({id: aSegment.id, blobHTML: selection});
+
+		if (range && range.commonAncestorContainer){
+			// Make sure we record this change in order to give the possibility of reversing it.
+			undoStack.push({id: range.commonAncestorContainer.id, which: "succession", blobHTML: range.commonAncestorContainer.outerHTML});
+		}	
+
 		console.log('Last on undostack is ', undoStack[undoStack.length - 1]);
 		console.log('wrapper is', wrapper);
 		console.log('range is ', range);
+		
+
+		console.log('range commonancestor is ', range.commonAncestorContainer);
+		console.log('selection is ', selection);
 		console.log(undoStack);
 	}
 
@@ -139,54 +193,45 @@
 	};
 
 
-	// This function is to ensure we have an initial record in the array prior to when we first click into a selection.
-	function invokeFirstSegmentRecord(obj){
-		let grabbedInnerDivById = document.getElementById('0');
-		undoStack.push({ 
-			id: obj.id, 
-			blobHTML: obj.blobHTML 
-		});
-	}
-
 	const processOnMouseUp = function(event) {
 
 		// Get the element where the cursor is located
 		const target = event.target;
-
+		
+		// This following is to ensure we have an initial record in the array prior to when we first click into a selection.
 		if (!singletonVarOnMouseUp){
-			invokeFirstSegmentRecord({
+			undoStack.push({ 
 				id: target.id, 
-				blobHTML: target.outerHTML
-				});
-			singletonVarOnMouseUp = true;
-		}
+				blobHTML: target.outerHTML 
+			});
 
-		// If this is the first time you enter the box, and the previous box has been changed by user input, 
-		// and the present box has class="segment"
-		if (target.id !== singletonVarBoxEntered && boxIsChanged && target.classList.contains('segment')) {
-			// Set the present box to unchanged
-			boxIsChanged = false;
-			const _aSegment = {
-				id: target.id, 
-				blobHTML: target.outerHTML
+			singletonVarOnMouseUp = true;
+		} else {
+			// If this is the first time you enter the present box, and the previous box has been changed by user input, do this.
+			// (The subsequent times you enter the present box won't affect anything, even if the present box becomes changed.)
+			if (target.id !== singletonVarBoxEntered && boxIsChanged) {
+				// Set the present box
+				const _aSegment = {
+					id: target.id, 
+					blobHTML: target.outerHTML
 				};
-			// Here we make the copy as aSegment an immutable copy of _aSegment
-			aSegment = { ... _aSegment };
-			// Here again we cause an immutable copy to be stored in the undoStack array
-			undoStack.push({ ... _aSegment});
-			console.log(undoStack);
-		} 
+				// Here we make the global copy as aSegment an immutable copy of _aSegment
+				aSegment = { ... _aSegment };
+				// Here we cause an immutable copy of the present box to be stored upon the undoStack array
+				undoStack.push({ ... _aSegment});
+				// Now record that fact that the present box is unchanged.
+				boxIsChanged = false;
+			} 
+		}
 		
 		// Update singletonVarBoxEntered to ensure that subsequent clicks on the same box won't change anything until a 
-		// different box is clicked on.
+		// different box is clicked on.  singletonVarBoxEntered is a global variable
 		singletonVarBoxEntered = target.id;	
-
+		
 		//  Here we set topOfStack=true every time we add something to it.  See processOnUndoButton for use of this variable
 		if (!topOfStack){
 			topOfStack = true;
 		} 
-		
-
 	};
 
     function addSegmentToSplurge(mytext, id) {
@@ -228,28 +273,37 @@
 		const lastOnStack = undoStack[undoStack.length - 1];
 		console.log(undoStack);
 		if (undoStack.length>0){
-			// if the last on stack is from a formatting change
-			if (topOfStack) {
-				// Fires on time on each first time of pop.  Logic is for smooth user 
-				// experience depending upon where they left off when undo was pressed
-				console.log('FIDDLESTICKS');
-				if (boxIsChanged) {
-					console.log("BOX IS CHANGED");
-					performUndoEventTextChange();
-				} else {
-					undoStack.pop();
-					performUndoEventTextChange();
-				}
+			if (lastOnStack.selection instanceof Selection ) {
+				if (topOfStack) {
+					// Fires on time on each first time of pop.  Logic is for smooth user 
+					// experience depending upon where they left off when undo was pressed
 
-			} else {
-				if (lastOnStack.blobHTML instanceof Selection ) {
 					// the last on stack was from a text alteration
 					console.log('A b/it/u alteration');
-					performUndoEventFormat();
-				} else { 
-					console.log('A text change');
-					performUndoEventTextChange();	
-				}		
+					if (boxIsChanged) {
+						console.log("BOX IS CHANGED");
+						performUndoEventFormat();
+					} else {
+						undoStack.pop();
+						console.log('From else');
+						performUndoEventFormat();
+					}
+				} 		
+
+			} else { 
+				// if the last on stack is from a text alteration change
+				if (topOfStack) {
+					// Fires on time on each first time of pop.  Logic is for smooth user 
+					// experience depending upon where they left off when undo was pressed
+					console.log('A text alteration ');
+					if (boxIsChanged) {
+						console.log("BOX IS CHANGED");
+						performUndoEventTextChange();
+					} else {
+						undoStack.pop();
+						performUndoEventTextChange();
+					}
+				} 				
 			}
 		}
 
@@ -264,14 +318,14 @@
 
 	function performUndoEventFormat () {
 		console.log('undostack is ', undoStack);
-			let grabbedInnerDivById = document.getElementById(undoStack[undoStack.length - 1].id);
-			console.log('from DOM', grabbedInnerDivById);
-			console.log('From blobHTML ', grabbedInnerDivById);
+			let grabbedInnerDivByIdFromUndoStack = document.getElementById(undoStack[undoStack.length - 1].id);
+			console.log('from DOM performUndoEventFormat ', grabbedInnerDivByIdFromUndoStack);
+			console.log('From blobHTML performUndoEventFormat ', undoStack[undoStack.length - 1].blobHTML);
 			
 
-			grabbedInnerDivById = undoStack[undoStack.length - 1].blobHTML;
+			grabbedInnerDivByIdFromUndoStack.outerHTML = undoStack[undoStack.length - 1].blobHTML;
 			// Place cursor at the end
-			placeCursorAtEnd(splurgeContainer);
+			// placeCursorAtEnd(splurgeContainer);
 		}
 
 	function placeCursorAtEnd(el) {
@@ -286,11 +340,11 @@
 
 	function performUndoEventTextChange () {
 		console.log('From undostack ', undoStack);
-			let grabbedInnerDivById = document.getElementById(undoStack[undoStack.length - 1].id);
-			console.log('from DOM ', grabbedInnerDivById);
-			console.log('From blobHTML ', undoStack[undoStack.length - 1].blobHTML);
+			let grabbedInnerDivByIdFromUndoStack = document.getElementById(undoStack[undoStack.length - 1].id);
+			console.log('from DOM performUndoEventTextChange ', grabbedInnerDivByIdFromUndoStack);
+			console.log('From blobHTML performUndoEventTextChange ', undoStack[undoStack.length - 1].blobHTML);
 
-			grabbedInnerDivById.outerHTML = undoStack[undoStack.length - 1].blobHTML;
+			grabbedInnerDivByIdFromUndoStack.outerHTML = undoStack[undoStack.length - 1].blobHTML;
 		}
 
 }());
